2357f11f32a6a9bd57f7160090756349a94be629
Ramis03 <zhumanazarovramis@gmail.com>
2021-06-23 17:49:38 +0600

    first commit

---
+++ src/js/main.js
@@ -0,0 +1,142 @@
+//slideshow style interval
+$(document).ready(function () {
+
+var autoSwap = setInterval( swap,5000);
+
+//pause slideshow and reinstantiate on mouseout
+$('.items').hover(
+    function () {
+        clearInterval(autoSwap);
+    },
+    function () {
+        autoSwap = setInterval( swap,5000);
+    });
+
+//global variables
+var items = [];
+var startItem = 1;
+var position = 0;
+var itemCount = $('.carousel li.items').length;
+var leftpos = itemCount;
+var resetCount = itemCount;
+
+//unused: gather text inside items class
+$('li.items').each(function(index) {
+    items[index] = $(this).text();
+});
+
+//swap images function
+function swap(action) {
+    var direction = action;
+
+    //moving carousel backwards
+    if(direction === 'counter-clockwise') {
+        var leftitem = $('.left-pos').attr('id') - 1;
+        if(leftitem == 0) {
+            leftitem = itemCount;
+        }
+
+        $('.right-pos').removeClass('right-pos').addClass('back-pos');
+        $('.main-pos').removeClass('main-pos').addClass('right-pos');
+        $('.left-pos').removeClass('left-pos').addClass('main-pos');
+        $('#'+leftitem+'').removeClass('back-pos').addClass('left-pos');
+
+        startItem--;
+        if(startItem < 1) {
+            startItem = itemCount;
+        }
+    }
+
+    //moving carousel forward
+    if(direction == 'clockwise' || direction == '' || direction == null ) {
+        function pos(positionvalue) {
+            if(positionvalue != 'leftposition') {
+                //increment image list id
+                position++;
+
+                //if final result is greater than image count, reset position.
+                if((startItem+position) > resetCount) {
+                    position = 1-startItem;
+                }
+            }
+
+            //setting the left positioned item
+            if(positionvalue == 'leftposition') {
+                //left positioned image should always be one left than main positioned image.
+                position = startItem - 1;
+
+                //reset last image in list to left position if first image is in main position
+                if(position < 1) {
+                    position = itemCount;
+                }
+            }
+
+            return position;
+        }
+
+        $('#'+ startItem +'').removeClass('main-pos').addClass('left-pos');
+        $('#'+ (startItem+pos()) +'').removeClass('right-pos').addClass('main-pos');
+        $('#'+ (startItem+pos()) +'').removeClass('back-pos').addClass('right-pos');
+        $('#'+ pos('leftposition') +'').removeClass('left-pos').addClass('back-pos');
+
+        startItem++;
+        position=0;
+        if(startItem > itemCount) {
+            startItem = 1;
+        }
+    }
+}
+//next button click function
+$('#next').on('click', function() {
+    swap('clockwise');
+});
+
+//prev button click function
+$('#prev').on('click' ,function() {
+    swap('counter-clockwise');
+});
+
+//if any visible items are clicked
+$('li.items').on('click', function() {
+    if($(this).attr('class') === 'items left-pos') {
+        swap('counter-clockwise');
+    }
+    else {
+        swap('clockwise');
+    }
+});
+
+
+
+    $('.features__descriptionTitle').each(function (item) {
+        $(this).on('mouseover', function () {
+            $('.features__descriptionSubtitle').eq(item).addClass('features__descriptionSubtitle_active',5000)
+        });
+        $(this).on('mouseout', function () {
+            $('.features__descriptionSubtitle').eq(item).removeClass('features__descriptionSubtitle_active',5000)
+        })
+    });
+
+    $(window).on('scroll',function () {
+        if($(this).scrollTop() > 2000){
+            $('.goTop').fadeIn('slow')
+        }
+        else{
+            $('.goTop').fadeOut('slow')
+        }
+    });
+
+
+
+    $('#demoDefaultSelection').ddslick({
+        onSelected: function (selectedData) {
+            //callback function: do something with selectedData;
+        }
+    });
+
+    $(document).ready(function () {
+        $('.burger').on('click', function () {
+            $('.burger, .header__nav').toggleClass('active')
+        })
+    })
+});
